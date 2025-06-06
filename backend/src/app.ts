7c13e5478badf47efeaca9ec904eb08b62c6e37e
Satyam Sagar <satyamsagar30@gmail.com>
2025-06-06 16:50:50 +0530

    chore(server): config for deployment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,20 @@
 import dotenv from "dotenv";
-import { app } from "./app";
-import connectDB from "./db";
-
 dotenv.config();
 
-connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      return console.log(`Server is running at port: ${process.env.PORT}`);
+import connectDB from "./db";
+import { app } from "./app";
+
+if (process.env.NODE_ENV !== "production") {
+  connectDB()
+    .then(() => {
+      app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT || 8000}`);
+      });
+    })
+    .catch((error) => {
+      console.log("MONGO DB connection failed !!", error);
     });
-  })
-  .catch((error) => {
-    console.log("MONGO DB connection failed !!", error);
-  });
+}
+
+// Export for Vercel
+export default app;
